fb6a2e48183431fb43e61a35351538fd1c463135
Jason Rowe <jason.rowe@protolabs.com>
2020-05-12 16:49:40 -0500

    Adjust tests
    Fix path to work on linux

--- KestrelMock.Tests/MockTests.cs
+++ KestrelMock.Tests/MockTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Threading.Tasks;
 using KestrelMock.Tests.TestHelpers;
@@ -10,12 +12,16 @@ namespace KestrelMock.Tests
 {
 	public class MockTests
 	{
-		private const string HTTP_TEST_HOST = "http://localhost:60000/";
+		private const string HTTP_TEST_HOST = "http://localhost:60001/";
+
+		public MockTests()
+		{
+			KestrelMock.Run(BuildConfiguration(), new List<string> { HTTP_TEST_HOST });
+		}
 
 		[Fact]
 		public async Task CanStartup()
 		{
-			KestrelMock.Run(BuildConfiguration());
 			var response = await HttpHelper.GetAsync(HTTP_TEST_HOST);
 			Assert.True(response.HttpStatusCode == HttpStatusCode.NotFound);
 		}
@@ -36,9 +42,6 @@ namespace KestrelMock.Tests
 		[Fact]
 		public async Task CanMockResponseUsingPathStartsWith()
 		{
-			KestrelMock.Run(new ConfigurationBuilder()
-						.AddJsonFile("appsettings.json", optional: false)
-						.Build());
 			var response = await HttpHelper.GetAsync(HTTP_TEST_HOST + "starts/with/" + Guid.NewGuid());
 			Assert.Contains("banana_x", response.Content);
 			Assert.Equal(200, (int)response.HttpStatusCode);
@@ -47,9 +50,6 @@ namespace KestrelMock.Tests
 		[Fact]
 		public async Task CanMockResponseUsingPathRegex_Matches()
 		{
-			KestrelMock.Run(new ConfigurationBuilder()
-						.AddJsonFile("appsettings.json", optional: false)
-						.Build());
 			var response = await HttpHelper.GetAsync(HTTP_TEST_HOST + "/test/1234/xyz");
 			Assert.Contains("regex_is_working", response.Content);
 			Assert.Equal(200, (int)response.HttpStatusCode);
@@ -58,18 +58,14 @@ namespace KestrelMock.Tests
 		[Fact]
 		public async Task CanMockResponseUsingPathRegex_NoMatch()
 		{
-			KestrelMock.Run(new ConfigurationBuilder()
-						.AddJsonFile("appsettings.json", optional: false)
-						.Build());
 			var response = await HttpHelper.GetAsync(HTTP_TEST_HOST + "/test/abcd/xyz");
-			
+
 			Assert.Equal(HttpStatusCode.NotFound, response.HttpStatusCode);
 		}
 
 		[Fact]
 		public async Task CanMockGetResponseUsingExactPath()
 		{
-			KestrelMock.Run(BuildConfiguration());
 			var response = await HttpHelper.GetAsync(HTTP_TEST_HOST + "hello/world");
 			Assert.Contains("hello", response.Content);
 			Assert.Equal(200, (int)response.HttpStatusCode);
@@ -78,7 +74,6 @@ namespace KestrelMock.Tests
 		[Fact]
 		public async Task CanMockPosttResponseUsingExactPath()
 		{
-			KestrelMock.Run(BuildConfiguration());
 			var response = await HttpHelper.PostAsync(HTTP_TEST_HOST + "hello/world");
 			Assert.Contains("hello", response.Content);
 			Assert.Equal(200, (int)response.HttpStatusCode);
@@ -87,7 +82,6 @@ namespace KestrelMock.Tests
 		[Fact]
 		public async Task CanMockBodyContainsResponse()
 		{
-			KestrelMock.Run(BuildConfiguration());
 			var response = await HttpHelper.PostAsync(HTTP_TEST_HOST + "api/estimate", "00000");
 			Assert.True(response.Content == "BodyContains Works!");
 			Assert.Equal(200, (int)response.HttpStatusCode);
@@ -96,7 +90,6 @@ namespace KestrelMock.Tests
 		[Fact]
 		public async Task CanMockBodyDoesNotContainsResponse()
 		{
-			KestrelMock.Run(BuildConfiguration());
 			var response = await HttpHelper.PostAsync(HTTP_TEST_HOST + "api/estimate", "foo");
 			Assert.True(response.Content == "BodyDoesNotContain works!!");
 			Assert.Equal(200, (int)response.HttpStatusCode);
@@ -105,7 +98,6 @@ namespace KestrelMock.Tests
 		[Fact]
 		public async Task LoadBodyFromRelativePath()
 		{
-			KestrelMock.Run(BuildConfiguration());
 			var response = await HttpHelper.PostAsync(HTTP_TEST_HOST + "api/fromfile", "foo");
 			Assert.True(response.Content == "Body loaded from file");
 			Assert.Equal(200, (int)response.HttpStatusCode);
@@ -114,7 +106,6 @@ namespace KestrelMock.Tests
 		[Fact]
 		public async Task CanReturnErrorStatus()
 		{
-			KestrelMock.Run(BuildConfiguration());
 			var response = await HttpHelper.PostAsync(HTTP_TEST_HOST + "errors/502", "foo");
 			Assert.Equal(502, (int)response.HttpStatusCode);
 		}
@@ -122,12 +113,8 @@ namespace KestrelMock.Tests
 		[Fact]
 		public async Task KestralMock_works_with_Refit()
 		{
-			KestrelMock.Run(BuildConfiguration());
-
-			var testApi = RestService.For<IKestralMockTestApi>("http://localhost:60000");
-
+			var testApi = RestService.For<IKestralMockTestApi>(HTTP_TEST_HOST);
 			var helloWorld = await testApi.GetHelloWorldWorld();
-
 			Assert.Contains("world", helloWorld.Hello);
 		}
 
